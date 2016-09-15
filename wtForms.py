177d6152a4dddc78377d005d426a58cf6cbd7008
Vamsi krishna Nallabothu <vamsikrishna.nallabothu@sjsu.edu>
2016-09-15 02:59:58 -0700

    Commit 1
    Final Commit

---
+++ wtForms.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from flask_moment import Moment
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
+import os
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+app.config['SECRET_KEY'] = os.urandom(26)
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired])
+    password = PasswordField('Password', validators=[DataRequired])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    password = None
+    form = NameForm()
+    if form.validate_on_submit():
+            name = form.name.data
+            password = form.password.data
+    form.name.data = ''
+    form.password.data = ''
+    return render_template('index.html', form=form, name=name, password = password)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
